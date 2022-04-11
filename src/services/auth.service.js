6a256efab2603efe5cfb793b958e147aeb685b92
code_max <ogbujigodwinmax@gmail.com>
2022-04-11 22:46:58 +0100

    feature: added sign up feature

---
+++ src/services/auth.service.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const User = require("./../models/user.model");
+const AppError = require("./../utils/appError");
+
+
+  const signToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+  }
+
+  const createSendToken = (user) => {
+    const token = signToken(user);
+
+    user.password = undefined;
+
+    return {
+      status: "success",
+      token,
+      data: {
+        user: user,
+      },
+    };
+  }
+
+  exports.signUp = async (data, next) => {
+    let user = await User.findOne({ email: data.email });
+    if (user) next(new AppError("Email already exists", 400));
+    const newUser = await User.create({
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      phone: data.phone,
+      password: data.password,
+    });
+
+    return createSendToken(newUser);
+  }
+
+
