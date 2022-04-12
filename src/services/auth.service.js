f1a80db7def41cf460cca5aef68b3475d0a7055d
code_max <ogbujigodwinmax@gmail.com>
2022-04-12 09:45:22 +0100

    feature: added sign in feature

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -37,4 +37,22 @@ const AppError = require("./../utils/appError");
     return createSendToken(newUser);
   }
 
+  exports.signin = async (data, next) => {
+    const { email, password } = data;
+
+    // 1 check if email and password exists
+    if (!email || !password) {
+      return next(new AppError("Please provide email and password", 400));
+    }
+
+    //2 check if user exists and password is correct
+    const user = await User.findOne({ email: email }).select("+password");
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError("Incorrect email or password", 401));
+    }
+
+    // 3 Send token
+    return createSendToken(user);
+  };
+
 
