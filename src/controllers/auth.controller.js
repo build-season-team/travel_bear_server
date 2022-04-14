1ee69589b92e31bdcfdee11a726e76eee9142f13
code_max <ogbujigodwinmax@gmail.com>
2022-04-14 02:53:55 +0100

    fixed bug

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -25,7 +25,7 @@ const createSendToken = (user, statusCode, req, res) => {
 exports.register = catchAsync ( async (req, res, next) => {
   const data = req.body;
   let oldUser = await User.findOne({ email: data.email });
-  if (oldUser) next(new AppError("Email already exists", 400));
+  if (oldUser) return next(new AppError("Email already exists", 400));
   const newUser = await User.create({
     firstName: data.firstName,
     lastName: data.lastName,
@@ -42,13 +42,13 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 1 check if email and password exists
   if (!email || !password) {
-    next(new AppError("Please provide email and password", 400));
+    return next(new AppError("Please provide email and password", 400));
   }
 
   //2 check if user exists and password is correct
   const user = await User.findOne({ email: email }).select("+password");
   if (!user || !(await user.correctPassword(password, user.password))) {
-    next(new AppError("Incorrect email or password", 401));
+    return next(new AppError("Incorrect email or password", 401));
   }
 
   // 3 Send token
